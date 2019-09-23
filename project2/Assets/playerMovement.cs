2f07b8bc0dbbf6d8f89783e0332ed6a9a9009d7b
Manuel <manuelavalos7@yahoo.com>
2019-09-23 12:05:28 -0500

    initial commit

---
+++ project2/Assets/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movement_speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            //GetComponent<Animator>().SetTrigger("Walk");
+            transform.position += Time.deltaTime * transform.forward *movement_speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //GetComponent<Animator>().SetTrigger("Walk");
+            transform.position += Time.deltaTime * -transform.right * movement_speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //GetComponent<Animator>().SetTrigger("Walk");
+            transform.position += Time.deltaTime * -transform.forward * movement_speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //GetComponent<Animator>().SetTrigger("Walk");
+            transform.position += Time.deltaTime * transform.right * movement_speed;
+        }
+    }
+}
