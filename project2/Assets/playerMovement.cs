ef34481e95fdafe70f6753c4116b95ee465349ec
unknown <manuelavalos7@yahoo.com>
2019-09-24 10:10:36 -0500

    added animations and character

---
+++ project2/Assets/playerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private float speed = 5f;
+    private Animator animate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animate = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += transform.forward * Time.deltaTime*speed;
+            animate.speed = 1;
+            animate.SetBool("walkForward", true);
+        }
+        else
+        {
+            animate.SetBool("walkForward", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            animate.SetBool("walkLeft", true);
+            transform.position += -transform.right * Time.deltaTime* speed;
+            animate.speed = 1;
+        }
+        else
+        {
+            animate.SetBool("walkLeft", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Time.deltaTime *-transform.forward * speed;
+            animate.speed = 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animate.SetBool("walkRight", true);
+            transform.position += Time.deltaTime * transform.right * speed;
+            GetComponentInChildren<Animator>().speed = 1;
+        }
+        else {
+            animate.SetBool("walkRight", false);
+        }
+    }
+}
